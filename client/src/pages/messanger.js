dd3ef697564e785aad253df845c9883d545b479c
GrigoriiMarilov <9jiraf@gmail.com>
2022-11-25 14:53:25 +0400

    chat
    chat

--- client/src/pages/messanger.js
+++ client/src/pages/messanger.js
@@ -2,8 +2,8 @@
 import { observer } from "mobx-react-lite"
 import React from "react"
 
-import ChatBar from "../components/ChatBar"
-import MesPanel from "../components/MesPanel"
+
+import MessangerComponent from "../components/MessangerComponent"
 
 
 
@@ -11,8 +11,7 @@ const Messanger = observer(() => {
 
 	return (
 		<div className="ChatPage">
-			<ChatBar />
-			<MesPanel />
+			<MessangerComponent />
 		</div>
 	)
 })
